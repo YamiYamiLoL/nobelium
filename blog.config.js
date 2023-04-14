f7dab244d4904cbaab4084f0d3b88d9b5ef1fbb9
YamiYamiLoL <128779714+YamiYamiLoL@users.noreply.github.com>
2023-04-15 00:22:44 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -51,7 +51,7 @@ const BLOG = {
       repo: ''
     },
     cusdisConfig: {
-      appId: '', // data-app-id
+      appId: '8ca1e5f0-a04a-4eee-811f-c17605f7ae17', // data-app-id
       host: 'https://cusdis.com', // data-host, change this if you're using self-hosted version
       scriptSrc: 'https://cusdis.com/js/cusdis.es.js' // change this if you're using self-hosted version
     }
